d61416794b594c757ee155519c9000f9df9ed99c
Kavitha Nair <kavitha.n2326@gmail.com>
2026-08-18 10:22:39 -0400

    week 13: add park relationship and admin

--- trails/admin.py
+++ trails/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from .models import Trail
+from .models import Park, Trail
+
+
+@admin.register(Park)
+class ParkAdmin(admin.ModelAdmin):
+    list_display = ("name", "region")
+    search_fields = ("name", "region")
 
 
 @admin.register(Trail)
 class TrailAdmin(admin.ModelAdmin):
     list_display = (
         "name",
+        "park",
         "distance_km",
         "elevation_gain",
         "difficulty",
